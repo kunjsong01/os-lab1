4d19d05cc0952269188836c1db55778f97cab3bb
kunjsong01 <kunjian90@gmail.com>
2024-06-05 22:16:28 +0100

    [find] experiment with fstat and dir detect

--- user/find.c
+++ user/find.c
@@ -1,6 +1,8 @@
 #include "kernel/types.h"
+#include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/fs.h"
+#include "kernel/fcntl.h"
 
 void
 find(char *path, char *target)
@@ -10,8 +12,46 @@ find(char *path, char *target)
 
   fd = open(".", 0);
   while(read(fd, &e, sizeof(e)) == sizeof(e)){
+    if(e.name[0] != '\0') {
+      // recursive call if found a directory
+      struct stat st;
+      int fdDirTest;
+      char buf[512], *p;
+
+      strcpy(buf, path);
+      p = buf+strlen(buf);
+      *p++ = '/';
+      memmove(p, e.name, DIRSIZ); // new path
+      p[DIRSIZ] = 0;
+      printf("Now checking path: %s\n", buf);
+      // now buf should contain the path
+
+      if(strlen(p) + 1 + DIRSIZ + 1 > sizeof buf){
+        printf("find: path too long\n");
+        break;
+      }
+
+      if((fdDirTest = open(buf, 0)) < 0){
+        printf("find: cannot open %s\n", buf);
+        continue;
+      }
+
+      if(fstat(fdDirTest, &st) < 0){
+        printf("find: cannot fstat %s\n", buf);
+        close(fdDirTest);
+        continue;
+      }
+
+      if(st.type == T_DIR) {
+        printf("Found dir! - %s\n", e.name);
+      }
+      else if(st.type == T_FILE || st.type == T_DEVICE) {
+        printf("Found non-dir! - %s\n", e.name);
+      }
+    }
+
     if(e.name[0] != '\0' && strcmp(target, e.name) == 0){
-      printf("%s\n", e.name);
+      printf("Got it! - %s\n", e.name);
     }
   }
   close(fd);
